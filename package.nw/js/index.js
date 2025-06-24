0f09f530a7e09abe7dd9e44b2ca18721ed5ba9d0
Howard <124030139+H-owar-d@users.noreply.github.com>
2025-06-24 16:48:13 +0800

    Update index.js

--- package.nw/js/index.js
+++ package.nw/js/index.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		url: "https://h-owar-d.github.io/SpBlocklyC_v3/package.nw/board/board.xml" ,
 		dataType: "xml",
 		timeout: 3000,
-		async: false,
+		async: true,
 		success: function(xml, textStatus) {
 			if (xml.firstChild) {
 				var Nodes = xml.firstChild.childNodes;
